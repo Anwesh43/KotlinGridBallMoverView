399751c999e6e620cd9bb01159af3bb184c0a518
Anwesh43 <anweshthecool0@gmail.com>
2018-03-24 11:40:32 +0530

    adding GridBallMoverView in activity

--- app/src/main/java/ui/anwesome/com/kotlingridballmoverview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlingridballmoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlingridballmoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.gridballmoverview.GridBallMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        GridBallMoverView.create(this)
     }
 }
