628fe0844741ba1fa8101419fc05ea9f4c675cd2
Anwesh43 <anweshthecool0@gmail.com>
2018-03-24 11:49:02 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlingridballmoverview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlingridballmoverview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlingridballmoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.gridballmoverview.GridBallMoverView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GridBallMoverView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
